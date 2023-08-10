a6a3b01c774157d23be11958182f0d1445b6b703
f-alhajeri-nearpay <f.alhajeri@nearpay.io>
2023-08-10 16:43:30 +0300

    receipt to image testing

---
+++ android/src/main/java/io/nearpay/flutter/plugin/operations/ReceiptToImageOperation.java
@@ -0,0 +1,19 @@
+package io.nearpay.flutter.plugin.operations;
+
+import java.util.Map;
+
+import io.nearpay.flutter.plugin.PluginProvider;
+import io.nearpay.flutter.plugin.sender.NearpaySender;
+
+public class ReceiptToImage extends BaseOperation{
+
+    public ReceiptToImage(PluginProvider provider) {
+        super(provider);
+    }
+
+    @Override
+    public void run(Map args, NearpaySender sender) {
+
+        Rece
+    }
+}
