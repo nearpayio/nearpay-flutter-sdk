d53cc521ea03c4a7351bde89c84a7d85478bcff6
faisal alhajeri <f.alhajeri@nearpay.io>
2023-11-01 15:56:32 +0300

    errors update

--- lib/errors/reconcile_error/reconcile_error_switch.dart
+++ lib/errors/reconcile_error/reconcile_error_switch.dart
@@ -1,17 +1,18 @@
+import 'package:nearpay_flutter_sdk/errors/error_codes.dart';
 import 'package:nearpay_flutter_sdk/errors/reconcile_error/reconcile_error.dart';
 
 ReconcileError getReconcileError(Map<String, dynamic> nativeResponse) {
   int status = nativeResponse['status'];
 
   switch (status) {
-    case 403:
+    case ErrorCodes.failureCode:
       return ReconcileFailureMessage.fromJson(nativeResponse);
-    case 401:
+    case ErrorCodes.authFailedCode:
       return ReconcileAuthenticationFailed.fromJson(nativeResponse);
-    case 404:
+    case ErrorCodes.invalidCode:
       return ReconcileInvalidStatus.fromJson(nativeResponse);
-    case 402:
-      return ReconcileInvalidStatus.fromJson(nativeResponse);
+    case ErrorCodes.generalFailureCode:
+      return ReconcileGeneralFailure.fromJson(nativeResponse);
   }
 
   throw "no Error with status ${status}";
